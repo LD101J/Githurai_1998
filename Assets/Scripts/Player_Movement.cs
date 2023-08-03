e062c7889c6b2a50e87ed9e6c547d2ea15ef3591
LD101 <jmuendongei@gmail.com>
2023-08-03 10:59:28 +0300

    Player_Model

---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] private Transform player_Transform;
+    [SerializeField] private float limit_Value;
+    #endregion
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Move_Player();
+        }
+    }
+
+    private void Move_Player()
+    {
+        float halfScreen = Screen.width / 2;
+        float xPos = (Input.mousePosition.x - halfScreen) / halfScreen;
+        float finalXPos = Mathf.Clamp(xPos * limit_Value, -limit_Value, limit_Value);
+        player_Transform.localPosition = new Vector3(xPos, 1, 0);  
+    }
+}
